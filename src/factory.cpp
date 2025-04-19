174d2bab7f8525db60c5d3e316e7098ebba6ef1e
root <root@Rabbys-pc>
2025-04-19 14:52:16 +0600

    third commit

--- src/factory.cpp
+++ src/factory.cpp
@@ -15,17 +15,62 @@ public:
     virtual ~Scrollbar() = default;
 };
 
+
 class WindowsWindow : public Window {
-    public:
-        std::string render() const override {
-            return "Rendering Windows Window";
-        }
-    };
-    
-    class WindowsScrollbar : public Scrollbar {
-    public:
-        std::string render() const override {
-            return "Rendering Windows Scrollbar";
-        }
-    };
-    
+public:
+    std::string render() const override {
+        return "Rendering Windows Window";
+    }
+};
+
+class WindowsScrollbar : public Scrollbar {
+public:
+    std::string render() const override {
+        return "Rendering Windows Scrollbar";
+    }
+};
+
+
+class LinuxWindow : public Window {
+public:
+    std::string render() const override {
+        return "Rendering Linux Window";
+    }
+};
+
+class LinuxScrollbar : public Scrollbar {
+public:
+    std::string render() const override {
+        return "Rendering Linux Scrollbar";
+    }
+};
+
+
+class GUIFactory {
+public:
+    virtual std::unique_ptr<Window> createWindow() const = 0;
+    virtual std::unique_ptr<Scrollbar> createScrollbar() const = 0;
+    virtual ~GUIFactory() = default;
+};
+
+
+class WindowsFactory : public GUIFactory {
+public:
+    std::unique_ptr<Window> createWindow() const override {
+        return std::make_unique<WindowsWindow>();
+    }
+    std::unique_ptr<Scrollbar> createScrollbar() const override {
+        return std::make_unique<WindowsScrollbar>();
+    }
+};
+
+
+class LinuxFactory : public GUIFactory {
+public:
+    std::unique_ptr<Window> createWindow() const override {
+        return std::make_unique<LinuxWindow>();
+    }
+    std::unique_ptr<Scrollbar> createScrollbar() const override {
+        return std::make_unique<LinuxScrollbar>();
+    }
+};
